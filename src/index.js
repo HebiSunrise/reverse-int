18be0d5dc5a4f4e1fa4bbd32a753cb9aa405bf51
HebiSunrise <93012812+HebiSunrise@users.noreply.github.com>
2022-02-08 23:22:50 +0300

    add: first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(Math.abs(n));
+    let end = "";
+    for (let i = arr.length-1; i>=0; i--) {
+        end += arr[i];
+    }
+    return end
 }
